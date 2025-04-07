370199952b73049f3830ff82f1db7859cf8e0915
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-04-08 01:20:15 +0530

    Organizing files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import TheWelcome from './components/TheWelcome.vue'
-// import count from '@/components/count.vue'
-// import Hello from '@/components/Hello.vue'
+// import count from '@/components/02_Examples/count.vue'
+// import Hello from '@/components/02_Examples/Hello.vue'
 
-import Header from '@/components/Header.vue'
-import Body from '@/components/Body.vue'
-import Footer from '@/components/Footer.vue'
+import Header from '@/components/01_demo_page/Header.vue'
+import Body from '@/components/01_demo_page/Body.vue'
+import Footer from '@/components/01_demo_page/Footer.vue'
 
 export default{
   components:{
@@ -20,27 +18,34 @@ export default{
 </script>
 
 <template>
-  <!-- <h1>Hello from Soumita</h1>
-  <count/> -->
-
-  <!-- <Hello/> -->
-
+  <!-- Components for 01_demo_page starts -->
   <Header/>
   <Body/>
   <Footer/>
+  <!-- Components for 01_demo_page ends -->
+
+<!-- Components for 02_Examples starts -->
+  <!-- <h1>Hello from Soumita</h1>
+  <count/> -->
+  <!-- <Hello/> -->
+<!-- Components for 02_Examples ends -->
+
 </template>
 
 <style>
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  ::-webkit-scrollbar{
-    background-color: black;
-    width: 1px;
-  }
-  ::-webkit-scrollbar-thumb{
-    background-color: rgb(0, 0, 0);
-  }
+/* Style for 01_demo_page starts */
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+::-webkit-scrollbar{
+  background-color: black;
+  width: 1px;
+}
+::-webkit-scrollbar-thumb{
+  background-color: rgb(0, 0, 0);
+}
+
+/* Style for 01_demo_page ends */
 </style>
