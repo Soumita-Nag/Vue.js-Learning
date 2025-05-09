c5d2f94c1857e4bb33e3843089af12c47da0868a
Soumita Nag <168756525+Soumita-Nag@users.noreply.github.com>
2025-05-09 22:33:35 +0530

    v-show

--- src/components/05_conditional_rendering/vif.vue
+++ src/components/05_conditional_rendering/vif.vue
@@ -3,12 +3,15 @@
     <h1 v-else-if="cnt==1">Good Morning</h1>
     <h1 v-else>Bye Guys</h1>
     <button @click="toggle">toggle</button>
+
+    <h1 v-show="isActive">Hey there!! </h1>
+    <!-- v-show only hides the element, ie display none. but does not remove the element from the DOM. Whereas v-if removes the element from the dom completely. v-if comes with v-else-if ans v-else tag where v-show doesnt have those  -->
 </template>
 <script>
     export default{
         data(){
             return{
-                // isActive:true,
+                isActive:true,
                 cnt : 0,
             }
         },
@@ -17,6 +20,7 @@
                 this.cnt++;
                 if(this.cnt>2)
                 this.cnt=0;
+                this.isActive=!this.isActive;
             }
         }
     }
